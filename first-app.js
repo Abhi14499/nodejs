c54d1a5f4a26b88a483e8c0d1e65cdfb9789c55e
Abhi14499 <abhimishra14499@gmail.com>
2023-10-06 19:17:54 +0530

    task 2

--- first-app.js
+++ first-app.js
@@ -1,1 +1,94 @@
-console.log("Hello From Node.js");
+// console.log("Hello From Node.js");
+
+// const fruits = ['apple', 'oranges', ' ', 'mango', ' ', 'lemon'];
+
+// console.log(fruits.map(fruit => {
+//     if (fruit === ' ') {
+//         fruit = 'empty string'
+//     }
+
+//     return fruit;
+// }))
+
+
+// const obj1 = { 'key1': 1, 'key2': 2 }
+
+// const obj2 = { ...obj1, key1: 1000 }
+
+
+
+// console.log(obj1)
+
+// console.log(obj2)
+
+{
+    const obj1 = { 'key1': 1, "key2": 2, "key3": 1000 }
+
+    const { key1, key3 } = { ...obj1 }
+
+
+
+    console.log(key1, key3)
+
+}
+{
+    const arr1 = ['value1', 'value2']
+
+    const [val1, val2] = arr1
+
+
+
+    console.log(val1)
+
+    console.log(val2)
+
+
+} {
+    const obj1 = { 'key1': 1, "key2": 2, "key3": 1000 }
+
+    let { key1, key3 } = obj1
+
+
+
+    key1 = 20;
+
+    key3 = 123
+
+    console.log(obj1.key1, obj1.key3)
+}
+// console.log('a');
+
+// console.log('b');
+
+// setTimeout(() => console.log('c'), 3000)
+
+// setTimeout(() => console.log('d'), 0)
+
+// console.log('e');
+
+// const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+// async function printSequence() {
+//     console.log('a');
+//     console.log('b');
+
+//     await delay(3000);
+//     console.log('c');
+//     console.log('d');
+
+//     await delay(0);
+//     console.log('e');
+// }
+
+// printSequence();
+
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    console.log('Abhishek Mishra');
+    console.log(req)
+
+})
+
+server.listen(4000);
--- reqRes.js
+++ reqRes.js
@@ -2,13 +2,27 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
-    // if (url === '/') {
-    //     res.write('<html>');
-    //     res.write('<head><title>Enter Message</title></head>');
-    //     res.write('<body><form action = "/message" method "POST"><input type = "text" name = "message"</form></body>');
-    //     res.write('</html');
-    //     return res.end();
-    // }
+    if (url === '/home') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>Welcome home</body>');
+        res.write('</html');
+        return res.end();
+    }
+    else if (url === '/about') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body> Welcome to About Us page</body>');
+        res.write('</html');
+        return res.end();
+    }
+    else if (url === '/node') {
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body>Welcome to my Node Js project</body>');
+        res.write('</html');
+        return res.end();
+    }
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
